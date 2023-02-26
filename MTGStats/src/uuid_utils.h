5fdc6b1b93baf5e13e7c5bc5f6cfd48fe56f9363
Rilazy <35211613+Rilazy@users.noreply.github.com>
2023-02-26 01:56:27 -0500

    Document uuid generation

--- MTGStats/src/uuid_utils.h
+++ MTGStats/src/uuid_utils.h
@@ -4,8 +4,10 @@
 #include <random>
 #include <array>
 
+// Basically just a Schwarz Counter, but boolean.
 static bool statics_initialized = false;
 
+// The global state for the RNG part of UUID generation. This is basically copied straight from the docs of the uuid library https://github.com/mariusbancila/stduuid#:~:text=Creating%20a%20new%20UUID%20with%20a%20default%20random%20generator
 static std::random_device rd;
 static std::array<int, std::mt19937::state_size> seed_data = std::array<int, std::mt19937::state_size> {};
 static std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
@@ -13,7 +15,10 @@ static std::mt19937 generator = std::mt19937(seq);
 static uuids::uuid_random_generator gen(generator);
 
 
-
+/**
+ * \brief Generates a new UUID, initializing the global UUID generator state automatically if needed.
+ * \return A new, unique UUID
+ */
 inline uuids::uuid gen_uuid() {
 	if (!statics_initialized) {
 		statics_initialized = true;
